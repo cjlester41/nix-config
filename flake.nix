0880fa87ecbc11c816aeed0bf7b6b686c90234f3
cjlester41 <149527318+cjlester41@users.noreply.github.com>
2025-12-02 22:37:26 -0800

    home manager pinned to stable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     # chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     stylix.url = "github:danth/stylix";  
 
@@ -11,7 +11,7 @@
     # };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
